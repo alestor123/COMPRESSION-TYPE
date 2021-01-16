94ab299ccabcedd5945578e804840fa77c6d3e9c
alestor123 <alestoraldous@gmail.com>
2021-01-16 13:40:34 +0530

    added main code

--- App.js
+++ App.js
@@ -1,2 +1,5 @@
 var fileType = require('file-type'),
 type = ['7z','bz2','gz','rar','tar','zip','xz','gz'];
+module.exports = name => {
+if(type.includes(fileType(name).ext)) return fileType(name).ext
+}
